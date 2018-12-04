884f2ff5fe3d177875ad5938e6fab347ea0b37ce
Eduardo Fiuza Cruz <eduardofiuza6@hotmail.com>
2018-12-04 01:33:51 -0200

    Create classifica_buscas.py

---
+++ classifica_buscas.py
@@ -0,0 +1,46 @@
+from sklearn.naive_bayes import MultinomialNB
+import pandas as pd
+
+df = pd.read_csv('buscas.csv')
+
+X_df = df[['home','busca','logado']]
+Y_df = df['comprou']
+
+Xdummies_df = pd.get_dummies(X_df)
+Ydummies_df = Y_df
+
+X = Xdummies_df.values
+Y = Ydummies_df.values
+
+porcentagem_de_treino = 0.9
+
+tamanho_de_treino = int(porcentagem_de_treino * len(Y))
+tamanho_de_teste = len(Y) - tamanho_de_treino
+
+treino_dados = X[:tamanho_de_treino]
+treino_marcacoes = Y[:tamanho_de_treino]
+
+teste_dados = X[-tamanho_de_teste:]
+teste_marcacoes = Y[-tamanho_de_teste:]
+
+modelo = MultinomialNB()
+modelo.fit(treino_dados, treino_marcacoes)
+
+resultado = modelo.predict(teste_dados)
+diferencas = resultado - teste_marcacoes
+
+acertos = [d for d in diferencas if d == 0]
+
+total_de_acertos = len(acertos)
+total_de_elementos = len(teste_dados)
+
+porcentagem_de_acertos = 100 * total_de_acertos / total_de_elementos
+
+print("{}%".format(porcentagem_de_acertos))
+
+
+
+
+
+
+
