ca5a33f0a6f6ef99ca8034fd502a6438318b80d8
Eduardo Fiuza Cruz <eduardofiuza6@hotmail.com>
2018-12-05 04:31:07 -0200

    Update classifica_buscas.py

--- classifica_buscas.py
+++ classifica_buscas.py
@@ -1,94 +1,95 @@
-from sklearn.naive_bayes import MultinomialNB
+from collections import Counter
+from sklearn.model_selection import cross_val_score
+import numpy as np
 import pandas as pd
 
-from sklearn.ensemble import AdaBoostClassifier
-from collections import Counter
+df = pd.read_csv('situacao_do_cliente.csv')
+X_df = df[['recencia','frequencia', 'semanas_de_inscricao']]
+Y_df = df['situacao']
 
-#teste inicial: home, busca, logado => comprou
-#home, busca
-#home, logado
-#busca, logado
-#busca: 85.71% (7 testes)
-
-def fit_and_predict(nome, modelo, treino_dados, treino_marcacoes, teste_dados, teste_marcacoes):
-    # TREINANDO O ALGORITMO
-    modelo.fit(treino_dados, treino_marcacoes)
-
-    # TESTANDO O ALGORITMO
-    resultado = modelo.predict(teste_dados)
-    total_de_acertos = sum(resultado == teste_marcacoes)  # somando os valores true da comparaçao
-
-    # CALCULANDO A PORCENTAGEM DE ACERTO DO ALGORITMO
-    total_de_elementos = len(teste_dados)
-    porcentagem_de_acertos = (100 * total_de_acertos / total_de_elementos)
-    msg = "{} acertou {}%".format(nome, porcentagem_de_acertos)
-
-    # PRINTANDO RESULTADO
-    print(msg)
-    return porcentagem_de_acertos
-
-
-#DEFINICAO DE CONSTANTES
-porcentagem_de_treino = 0.8
-porcentagem_de_teste = 0.1
-porcentagem_de_validacao = 1 - (porcentagem_de_treino + porcentagem_de_teste)
-
-
-#ABERTURA DE ARQUIVOS E DEFINICAO DE DADOS E MARCADORES
-df = pd.read_csv('buscas.csv')
-X_df = df[['home','busca','logado']]
-Y_df = df['comprou']
-
-
-#TRATAMENTO DE DADOS COM DUMMIES
 Xdummies_df = pd.get_dummies(X_df)
 Ydummies_df = Y_df
 
 X = Xdummies_df.values
 Y = Ydummies_df.values
 
-#DEFININDO TAMANHO DO TREINO, TESTE E VALIDAÇAO E ATRIBUINDO SEUS DADOS
+porcentagem_de_treino = 0.8
+
 tamanho_de_treino = int(porcentagem_de_treino * len(Y))
-tamanho_de_teste = int(porcentagem_de_teste * len(Y))
-tamanho_de_validacao = int(porcentagem_de_validacao * len(Y))
+tamanho_de_validacao = len(Y) - tamanho_de_treino
 
-treino_dados = X[0:(tamanho_de_treino )]
-treino_marcacoes = Y[0:(tamanho_de_treino)]
+treino_dados = X[:tamanho_de_treino]
+treino_marcacoes = Y[:tamanho_de_treino]
 
-fim_de_teste = tamanho_de_treino + tamanho_de_teste
-
-teste_dados = X[tamanho_de_treino:fim_de_teste]
-teste_marcacoes = Y[tamanho_de_treino:fim_de_teste]
+validacao_dados = X[tamanho_de_treino:]
+validacao_marcacoes = Y[tamanho_de_treino:]
 
 
-validacao_dados = X[fim_de_teste:]
-validacao_marcacoes = Y[fim_de_teste:]
 
 
-numero_de_teste = len(validacao_marcacoes)
 
+def fit_and_predict(nome, modelo, treino_dados, treino_marcacoes):
+    k = 10
+    scores = cross_val_score(modelo, treino_dados, treino_marcacoes, cv=k)
 
-#TESTE DO ALGORITMO BASE
-acertos_base = max(Counter(teste_marcacoes).values()) #quantidade de vezes que o que mais se repete aparece
-porcentagem_de_acertos_base = 100 * (acertos_base/len(validacao_marcacoes))
+    taxa_de_acerto = 100.0 * np.mean(scores)
+    msg = "Taxa de acerto do algoritmo {0}: {1}".format(nome, taxa_de_acerto)
+    print(msg)
 
+    return taxa_de_acerto
 
-#MAIN
+def teste_real(modelo, validacao_dados, validacao_marcacoes):
+    vencedor.fit(treino_dados, treino_marcacoes)
+    resultado = modelo.predict(validacao_dados)
+
+    acertos = resultado == validacao_marcacoes
+
+    total_de_acertos = sum(acertos)
+    total_de_elementos = len(validacao_marcacoes)
+
+    taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+
+    msg = "Taxa de acerto do vencedor entre os dois algoritmos no mundo real: {0}".format(taxa_de_acerto)
+    print(msg)
+
+resultados = {}
+
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC
+modeloOneVsRest = OneVsRestClassifier(LinearSVC(random_state = 0))
+resultadoOneVsRest = fit_and_predict("OneVsRest", modeloOneVsRest, treino_dados,
+     treino_marcacoes)
+resultados[resultadoOneVsRest] = modeloOneVsRest
+
+from sklearn.multiclass import OneVsOneClassifier
+modeloOneVsOne = OneVsOneClassifier(LinearSVC(random_state=0))
+resultadoOneVsOne = fit_and_predict("OneVsOne", modeloOneVsOne, treino_dados,
+     treino_marcacoes)
+resultados[resultadoOneVsOne] = modeloOneVsOne
+
+from sklearn.naive_bayes import MultinomialNB
 modeloMultinomial = MultinomialNB()
-algoritmo1 = fit_and_predict("MultinomialNB", modeloMultinomial, treino_dados, treino_marcacoes, teste_dados, teste_marcacoes)
+resultadoMultinomial = fit_and_predict("MultinomialNB", modeloMultinomial, treino_dados,
+     treino_marcacoes)
+resultados[resultadoMultinomial] = modeloMultinomial
 
+from sklearn.ensemble import AdaBoostClassifier
 modeloAdaBoost = AdaBoostClassifier()
-algoritmo2 = fit_and_predict("AdaBoostClassifier", modeloAdaBoost, treino_dados, treino_marcacoes, teste_dados, teste_marcacoes)
+resultadoAdaBoost = fit_and_predict("AdaBoostClassifier", modeloAdaBoost, treino_dados,
+     treino_marcacoes)
+resultados[resultadoAdaBoost] = modeloAdaBoost
 
-if algoritmo1 >= algoritmo2:
-    vencedor =  modeloMultinomial
-else:
-    vencedor = modeloAdaBoost
+maximo = max(resultados)
+vencedor = resultados[maximo]
 
-vencedor_predict = vencedor.predict(validacao_dados)
-acertos_vencedor = sum(vencedor_predict == validacao_marcacoes)
-porcentagem_acerto_validacao = 100 * (acertos_vencedor/len(validacao_marcacoes))
-print("Porcentagem de acertos na validação: {}".format(porcentagem_acerto_validacao))
+print("Vencedor: ")
+print(vencedor)
 
-print("Algoritmo base: {}%".format(porcentagem_de_acertos_base))
-print("Numero de testes: {}".format(numero_de_teste))
+teste_real(vencedor, validacao_dados, validacao_marcacoes)
+
+acerto_base = max(Counter(validacao_marcacoes).values())
+taxa_de_acerto_base = 100.0 * acerto_base / len(validacao_marcacoes)
+print("Taxa de acerto base: %f" % taxa_de_acerto_base)
+
+total_de_elementos = len(validacao_dados)
+print("Total de teste: %d" % total_de_elementos)
